ebd38013830eb02cdc08a0d319e1a8d6da776021
EmpiresDesMachines <empiresdesmachines@gmail.com>
2021-11-19 12:33:23 +0300

    add coeff to convert

--- src/commands/convert.js
+++ src/commands/convert.js
@@ -5,19 +5,30 @@ const Discord = require("discord.js");
 module.exports = async (msg) => {
   const { content: message } = msg;
   let currency = message.split(" ").slice(1);
-  if (currency.length !== 2) {
+  if (currency.length < 2 || currency.length > 3) {
     return msg.channel.send("Укажите два аргумента! *(например: !convert USD EUR)*");
   }
-  let [from, to] = currency; 
+  let coeff = 1;
+  let from = "";
+  let to = "";
+  if (currency.length == 2) {
+    [from, to] = currency;
+  }
+  if (currency.length == 3) {
+    [n, from, to] = currency;
+    coeff = +n;
+  }
+  if (isNaN(coeff)) return msg.channel.send("Не верный формат данных! <:pepe:244531994808156161>"); 
   from = from.toUpperCase();
   to = to.toUpperCase();
   try {
       const response = await fetch(`https://free.currconv.com/api/v7/convert?q=${from}_${to}&compact=ultra&apiKey=${process.env.CURRCONV_API_KEY}`);
       const json = await response.json();
       let [value] = Object.values(json);
-      if (!value) return msg.channel.send("Некорректные пользовательские данные! <:pepe:244531994808156161>"); 
-      value = parseInt(value * 100) / 100;
-      return msg.channel.send(`1 ${from} = ${value} ${to} <:frymoney:657429069620838422><:frymoney:657429069620838422><:frymoney:657429069620838422>`); 
+      if (!value) return msg.channel.send("Некорректные пользовательские данные! <:pepe:244531994808156161>");
+      value *= coeff;
+      value = value >= 1 ? parseInt(value * 100) / 100 : value.toPrecision(2);
+      return msg.channel.send(`${coeff} ${from} = ${value} ${to} <:frymoney:657429069620838422><:frymoney:657429069620838422><:frymoney:657429069620838422>`); 
 
   } catch (error) {
       return msg.channel.send("Ошибка! <:pepe:244531994808156161>");
